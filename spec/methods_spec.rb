90c7455c46caf1c4dd1a455a43045fd78da0f26e
fascilio <fascilior@gmail.com>
2023-05-15 16:07:32 +0300

    passed tests

--- spec/methods_spec.rb
+++ spec/methods_spec.rb
@@ -52,3 +52,28 @@ describe '#halve' do
   end
 
 end
+
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(a, b)
+  a + b
+end
+
+def halve(num)
+  if num.is_a?(Integer)
+    num / 2
+  else
+    nil
+  end
+end
